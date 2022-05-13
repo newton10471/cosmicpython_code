4d7a0d6920ac1cce20bbb51ad4b494b79d519ec7
Matt Newton <mnewton15@bloomberg.net>
2022-05-12 20:10:32 -0400

    first 3 tests passing

--- model.py
+++ model.py
@@ -1,15 +1,18 @@
 from dataclasses import dataclass
 from typing import List
 
+
 @dataclass
 class OrderLine:
   sku: str
   qty: int
 
+
 @dataclass
 class Order:
   reference_id: int
   lines: List[OrderLine]
+
 
 @dataclass
 class Batch:
@@ -17,7 +20,7 @@ class Batch:
   sku: str
   qty: int
 
-  def allocate_order_line(self, order_line):
+  def allocate(self, order_line):
     if self.sku == order_line.sku and self.qty >= order_line.qty:
       self.qty -= order_line.qty
       return True
--- test_model.py
+++ test_model.py
@@ -7,27 +7,43 @@ today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
-
-def test_allocating_to_a_batch_reduces_the_available_quantity():
-    sku = '12345'
-    batch_qty = 10
-    order_line_qty = 9
+def run_allocation(sku='12345',batch_qty=0,order_line_qty=0):
     batch = Batch(reference_id=1, sku=sku, qty=batch_qty)
     order_line = OrderLine(sku=sku, qty=order_line_qty)
-    batch.allocate_order_line(order_line)
+    if (batch.allocate(order_line) == True):
+        return batch
+    else:
+        return False
+
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch_qty = 10
+    order_line_qty = 9
+    batch = run_allocation(sku='12345', batch_qty=batch_qty, order_line_qty=order_line_qty)
     assert(batch.qty == batch_qty - order_line_qty)
 
+
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail("todo")
+    batch_qty = 10
+    order_line_qty = 9
+    batch = run_allocation(sku='12345', batch_qty=batch_qty, order_line_qty=order_line_qty)
+    assert(batch.qty != batch_qty)
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail("todo")
+    sku='12345'
+    batch_qty = 9
+    order_line_qty = 10
+    batch = Batch(reference_id=1, sku=sku, qty=batch_qty)
+    order_line = OrderLine(sku=sku, qty=order_line_qty)
+    assert(batch.allocate(order_line) == False)
 
-
+    
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail("todo")
-
+    batch_qty = 10
+    order_line_qty = 10
+    assert(run_allocation(sku='12345', batch_qty=batch_qty, order_line_qty=order_line_qty) == True)
+    
 
 def test_prefers_warehouse_batches_to_shipments():
     pytest.fail("todo")
