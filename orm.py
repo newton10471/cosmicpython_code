3ea71d461e8b808095955ee180f15b428fae3bdc
Matt Newton <mnewton15@bloomberg.net>
2022-05-21 10:10:18 -0400

    first 2 tests passing

--- orm.py
+++ orm.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import mapper, relationship
 
 import model
--- test_orm.py
+++ test_orm.py
@@ -4,7 +4,7 @@ import pytest
 
 def test_orderline_mapper_can_load_lines(session):
   session.execute(
-    'INSERT INTO order_lines (order, sku, qty) VALUES '
+    'INSERT INTO order_lines (orderid, sku, qty) VALUES '
     '("order1", "RED-CHAIR", 12),'
     '("order1", "RED-TABLE", 13),'
     '("order2", "BLUE-LIPSTICK", 14)'
@@ -12,6 +12,14 @@ def test_orderline_mapper_can_load_lines(session):
   expected = [
     model.OrderLine("order1", "RED-CHAIR", 12),
     model.OrderLine("order1", "RED-TABLE", 13),
-    model.OrderLine("order1", "BLUE-LIPSTICK", 14)
+    model.OrderLine("order2", "BLUE-LIPSTICK", 14)
   ]
   assert session.query(model.OrderLine).all() == expected
+
+def test_orderline_mapper_can_save_lines(session):
+  new_line = model.OrderLine("order1", "DECORATIVE-WIDGET", 12)
+  session.add(new_line)
+  session.commit()
+
+  rows = list(session.execute('SELECT orderid, sku, qty FROM "order_lines"'))
+  assert rows == [("order1", "DECORATIVE-WIDGET", 12)]
