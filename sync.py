2cebf78e69c303bb2809023c98fe3500ec4b8568
Matt Newton <mnewton15@bloomberg.net>
2022-05-28 17:46:40 -0400

    2 tests passing with refactored sync.py

--- sync.py
+++ sync.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 BLOCKSIZE = 65536
 
+
 def hash_file(path):
   hasher = hashlib.sha1()
   with path.open("rb") as file:
@@ -18,33 +19,43 @@ def hash_file(path):
   return hasher.hexdigest()
 
 
-def sync(source, dest):
-  # Walk the source folder and build a dict of filenames and their hashes
-  source_hashes = {}
-  for folder, _, files in os.walk(source):
+def read_paths_and_hashes(root):
+  hashes = {}
+  for folder, _, files in os.walk(root):
     for fn in files:
-      source_hashes[hash_file(Path(folder) / fn)] = fn
+      hashes[hash_file(Path(folder) / fn)] = fn
+  return hashes
+
+def determine_actions(src_hashes, dst_hashes, src_folder, dst_folder):
+  for sha, filename in src_hashes.items():
+    if sha not in dst_hashes:
+      sourcepath = Path(src_folder) / filename
+      destpath = Path(dst_folder) / filename
+      yield 'copy', sourcepath, destpath
+    
+    elif dst_hashes[sha] != filename:
+      olddestpath = Path(dst_folder) / dst_hashes[sha]
+      newdestpath = Path(dst_folder) / filename
+      yield 'move', olddestpath, newdestpath
+
+  for sha, filename in dst_hashes.items():
+    if sha not in src_hashes:
+      yield 'delete', dst_folder / filename
   
-  seen = set()  # Keep track of the files we've found in the target
 
-  # Walk the target folder and get the filenames and hashes
-  for folder, _, files in os.walk(dest):
-    for fn in files:
-      dest_path = Path(folder) / fn
-      dest_hash = hash_file(dest_path)
-      seen.add(dest_hash)
+def sync(source, dest):
+  # imperative shell step 1, gather inputs
+  source_hashes = read_paths_and_hashes(source)
+  dest_hashes = read_paths_and_hashes(dest)
 
-      # if there's a file in target that's not in source, delete it
-      if dest_hash not in source_hashes:
-        dest_path.remove()
+  # step 2: call functional core
+  actions = determine_actions(source_hashes, dest_hashes, source, dest)
 
-      # if there's a file in target that has a different path in source,
-      # move it to the correct path
-      elif dest_hash in source_hashes and fn != source_hashes[dest_hash]:
-        shutil.move(dest_path, Path(folder) / source_hashes[dest_hash])
-
-  # for every file that appears in source but not target, copy the file to 
-  # the target
-  for src_hash, fn in source_hashes.items():
-    if src_hash not in seen:
-      shutil.copy(Path(source) / fn, Path(dest) / fn)
+  # imperative shell step 3, apply outputs
+  for action, *paths in actions:
+    if action == 'copy':
+      shutil.copyfile(*paths)
+    if action == 'move':
+      shutil.move(*paths)
+    if action == 'delete':
+      os.remove(paths[0])
