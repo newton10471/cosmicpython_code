4fbe599bedec3d1561f9919d9f844df02e9a4df0
Matt Newton <mnewton15@bloomberg.net>
2022-05-31 06:10:58 -0400

    testing edge to edge with fake fs, not working

--- sync.py
+++ sync.py
@@ -19,43 +19,29 @@ def hash_file(path):
   return hasher.hexdigest()
 
 
-def read_paths_and_hashes(root):
+def reader(root):
   hashes = {}
   for folder, _, files in os.walk(root):
     for fn in files:
       hashes[hash_file(Path(folder) / fn)] = fn
   return hashes
-
-def determine_actions(src_hashes, dst_hashes, src_folder, dst_folder):
-  for sha, filename in src_hashes.items():
-    if sha not in dst_hashes:
-      sourcepath = Path(src_folder) / filename
-      destpath = Path(dst_folder) / filename
-      yield 'copy', sourcepath, destpath
-    
-    elif dst_hashes[sha] != filename:
-      olddestpath = Path(dst_folder) / dst_hashes[sha]
-      newdestpath = Path(dst_folder) / filename
-      yield 'move', olddestpath, newdestpath
-
-  for sha, filename in dst_hashes.items():
-    if sha not in src_hashes:
-      yield 'delete', dst_folder / filename
   
 
-def sync(source, dest):
-  # imperative shell step 1, gather inputs
-  source_hashes = read_paths_and_hashes(source)
-  dest_hashes = read_paths_and_hashes(dest)
+def sync(reader, filesystem, source_root, dest_root):
+  source_hashes = reader(source_root)
+  dest_hashes = reader(dest_root)
 
-  # step 2: call functional core
-  actions = determine_actions(source_hashes, dest_hashes, source, dest)
+  for sha, filename in source_hashes.items():
+    if sha not in dest_hashes:
+      sourcepath = source_root / filename
+      destpath = dest_root / filename
+      filesystem.copy(destpath, sourcepath)
+    
+    elif dest_hashes[sha] != filename:
+      olddestpath = dest_root / dest_hashes[sha]
+      newdestpath = dest_root / filename
+      filesystem.move(olddestpath, newdestpath)
 
-  # imperative shell step 3, apply outputs
-  for action, *paths in actions:
-    if action == 'copy':
-      shutil.copyfile(*paths)
-    if action == 'move':
-      shutil.move(*paths)
-    if action == 'delete':
-      os.remove(paths[0])
+  for sha, filename in dest_hashes.items():
+    if sha not in source_hashes:
+      filesystem.delete(dest_root/filename)
--- test_sync.py
+++ test_sync.py
@@ -1,19 +1,37 @@
 import tempfile
-from sync import determine_actions
+from sync import sync
 import pytest
 import shutil
 from pathlib import Path
 
+class FakeFileSystem(list):
+
+  def copy(self, src, dest):
+    self.append(('COPY', src, dest))
+
+  def move(self, src, dest):
+    self.append(('MOVE', src, dest))
+
+  def delete(self, src, dest):
+    self.append(('DELETE', src, dest))
+
 
 def test_when_a_file_exists_in_the_source_but_not_the_destination():
-  src_hashes = {'hash1': 'fn1'}
-  dst_hashes = {}
-  actions = determine_actions(src_hashes, dst_hashes, Path('/src'), Path('/dst'))
-  assert list(actions) == [('copy', Path('/src/fn1'), Path('/dst/fn1'))]
+  source = {'hash1': 'fn1'}
+  dest = {}
+  filesystem = FakeFileSystem()
+
+  reader = {"/source" : source, "/dest": dest}
+
+  print(reader)
+  
+  sync(reader.pop, filesystem, "/source", "/dest")
+
+  assert filesystem == [("MOVE", "/dest/original-file", "/dest/renamed-file")]
 
 
-def test_when_a_file_has_been_renamed_in_the_source():
-  src_hashes = {'hash1': 'fn1'}
-  dst_hashes = {'hash1': 'fn2'}
-  actions = determine_actions(src_hashes, dst_hashes, Path('/src'), Path('/dst'))
-  assert list(actions) == [('move', Path('/dst/fn2'), Path('/dst/fn1'))]
+# def test_when_a_file_has_been_renamed_in_the_source():
+#   src_hashes = {'hash1': 'fn1'}
+#   dst_hashes = {'hash1': 'fn2'}
+#   actions = determine_actions(src_hashes, dst_hashes, Path('/src'), Path('/dst'))
+#   assert list(actions) == [('move', Path('/dst/fn2'), Path('/dst/fn1'))]
